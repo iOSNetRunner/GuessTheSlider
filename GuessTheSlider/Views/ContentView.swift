3e85bac68c481e3c3a4f4dd8c3064d7aceb18246
NetRunner <galatskiy@gmail.com>
2023-07-29 19:05:04 +0300

    UISlider -> SwiftUI.

---
+++ GuessTheSlider/Views/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  GuessTheSlider
+//
+//  Created by Dmitrii Galatskii on 29.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var startNumber = Float.random(in: 1...100).rounded()
+    @State private var selectedValue: Float = 0
+    @State private var alertPresented = false
+    
+    var body: some View {
+        VStack {
+            Text("Move slider as close as possible to: \(startNumber.formatted())")
+            
+            HStack {
+                Text("0")
+                UISliderRepresentation(value: $selectedValue, action: computeScore)
+                Text("100")
+            }
+            .padding()
+            
+            Button {
+                alertPresented = true
+            } label: {
+                Text("Check me!")
+            }
+            .alert(isPresented: $alertPresented) {
+                Alert(title: Text("SCORE"), message: Text("\n🏆 Points scored: \(computeScore()) 🏆"))
+            }
+            .padding(.bottom, 20)
+            
+            Button {
+                startNumber = Float.random(in: 1...100).rounded()
+            } label: {
+                Text("Restart")
+            }
+        }
+        .padding()
+    }
+    
+    
+    private func computeScore() -> Int {
+        let targetValue = Double(startNumber)
+        let currentValue = Double(selectedValue)
+        let difference = abs(lround(targetValue) - lround(currentValue))
+        return Int(100 - difference)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessTheSlider/Views/UISliderRepresentation.swift
@@ -0,0 +1,69 @@
+//
+//  UISliderRepresentation.swift
+//  GuessTheSlider
+//
+//  Created by Dmitrii Galatskii on 29.07.2023.
+//
+
+import SwiftUI
+
+struct UISliderRepresentation: UIViewRepresentable {
+    
+    
+    @Binding var value: Float
+    
+    let action: () -> Int
+    
+
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider()
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.setValue(Float.random(in: 1...100).rounded(), animated: true)
+        
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueSelected),
+            for: .valueChanged)
+        
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        value = uiView.value
+        let opacity = CGFloat(Double(action())) / 100
+        uiView.thumbTintColor = .cyan.withAlphaComponent(opacity)
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+    
+    
+    
+}
+
+struct UISliderRepresentation_Previews: PreviewProvider {
+    static var previews: some View {
+        UISliderRepresentation(value: .constant(50)) {
+            30
+        }
+    }
+}
+
+
+extension UISliderRepresentation {
+    class Coordinator: NSObject {
+        @Binding var value: Float
+        
+        init(value: Binding<Float>) {
+            self._value = value
+        }
+        
+        @objc func valueSelected(_ sender: UISlider) {
+            value = sender.value
+        }
+    }
+}
