9ff427134a1c1b688e26070593700a2d7fea9871
NetRunner <galatskiy@gmail.com>
2023-07-29 12:20:45 +0300

    Initial Commit

---
+++ GuessTheSlider/GuessTheSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheSliderApp.swift
+//  GuessTheSlider
+//
+//  Created by Dmitrii Galatskii on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
